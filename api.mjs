bfe03dcbcb39d4b5f1ef4da7948e63e57c5e912e
Yuliia <iyulia1997len@gmail.com>
2024-05-21 15:57:18 +0300

    fixed post method

--- api.mjs
+++ api.mjs
@@ -5,107 +5,99 @@ import {
   rootHtmlTemplate,
   contacts,
   createHtmlTemplate
-} from "./data.mjs";
-import * as querystring from "node:querystring";
+} from './data.mjs'
+import * as querystring from 'node:querystring'
 
 const generateHtml = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(rootHtmlTemplate);
-};
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end(rootHtmlTemplate)
+}
 
 const generateAbout = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Plain text from HTTP server");
-};
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Plain text from HTTP server')
+}
 
 const generateContacts = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(generateContactsTemplate());
-};
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end(generateContactsTemplate())
+}
 
 const generateJson = (req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 200
 
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(contacts));
-};
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(contacts))
+}
 
 const generateForm = (req, res) => {
   if (!formTemplate) {
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Error: Form template not loaded");
+    res.statusCode = 500
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Error: Form template not loaded')
   } else {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end(createHtmlTemplate(formTemplate));
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(createHtmlTemplate(formTemplate))
   }
-};
+}
 
 const postData = (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader('Content-Type', 'text/plain')
 
-  if (req.headers["content-type"] === "application/x-www-form-urlencoded") {
-    let body = "";
+  if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    let body = ''
 
-    req.on("data", (chunk) => (body += chunk));
+    req.on('data', (chunk) => (body += chunk))
 
-    req.on("end", () => {
+    req.on('end', () => {
       try {
-        let contact = querystring.parse(body);
+        let contact = querystring.parse(body)
 
         contact = {
-          id: +contact["id"],
-          userName: contact["userName"],
-          completed: contact["completed"] === "on"
-        };
+          id: +contact['id'],
+          name: contact['name'],
+          completed: contact['completed'] === 'on'
+        }
 
-        contacts.push(contact);
+        contacts.push(contact)
 
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        res.end(generateContactsTemplate());
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.end(generateContactsTemplate())
       } catch (error) {
-        res.statusCode = 400;
-        res.end("Invalid form data");
+        res.statusCode = 400
+        res.end('Invalid form data')
       }
-    });
-  } else if (req.headers["content-type"] === "application/json") {
-    let dataJson = "";
+    })
+  } else if (req.headers['content-type'] === 'application/json') {
+    let dataJson = ''
 
-    req.on("data", (chunk) => (dataJson += chunk));
+    req.on('data', (chunk) => (dataJson += chunk))
 
-    req.on("end", () => {
+    req.on('end', () => {
       try {
-        contacts.push(JSON.parse(dataJson));
-        res.statusCode = 200;
-        res.end("Contact data was received");
+        contacts.push(JSON.parse(dataJson))
+        res.statusCode = 200
+        res.end('Contact data was received')
       } catch (error) {
-        res.statusCode = 400;
-        res.end("Invalid JSON data");
+        res.statusCode = 400
+        res.end('Invalid JSON data')
       }
-    });
+    })
   } else {
-    res.statusCode = 400;
-    res.end("Contact data must be in JSON format");
+    res.statusCode = 400
+    res.end('Contact data must be in JSON format')
   }
-};
+}
 
 const generateNotFound = (req, res) => {
-  res.statusCode = 404;
-  res.setHeader("Content-Type", "text/html");
-  res.end(notFoundHtmlTemplate);
-};
+  res.statusCode = 404
+  res.setHeader('Content-Type', 'text/html')
+  res.end(notFoundHtmlTemplate)
+}
 
-export {
-  generateHtml,
-  generateAbout,
-  generateJson,
-  generateNotFound,
-  generateForm,
-  generateContacts,
-  postData
-};
+export { generateHtml, generateAbout, generateJson, generateNotFound, generateForm, generateContacts, postData }
--- data.mjs
+++ data.mjs
@@ -1,4 +1,4 @@
-import fs from "node:fs/promises";
+import fs from 'node:fs/promises'
 
 const navigation = `
 <nav style="margin= 20px", align="center">
@@ -10,7 +10,7 @@ const navigation = `
   <a style="text-decoration: none" href="/form">Form</a>
   
 </nav>
-`;
+`
 
 const createHtmlTemplate = (htmlInjection) => `
       <!DOCTYPE html>
@@ -27,76 +27,69 @@ const createHtmlTemplate = (htmlInjection) => `
         </div>
       </body>
       </html>
-    `;
+    `
 
-const rootHtmlTemplate = createHtmlTemplate("<h1>Hello from HTTP server</h1>");
+const rootHtmlTemplate = createHtmlTemplate('<h1>Hello from HTTP server</h1>')
 
-const notFoundHtmlTemplate = createHtmlTemplate("<h1>404 Page Not Found</h1>");
+const notFoundHtmlTemplate = createHtmlTemplate('<h1>404 Page Not Found</h1>')
 
 const generateContactsTemplate = () => {
-  const headerHtml = `<h1>Contacts List</h1>`;
+  const headerHtml = `<h1>Contacts List</h1>`
   const contactsHtml = contacts
     .map(
       (contact) => `
         <div style="border-bottom: 1px solid #ccc; padding: 10px;">
           <p>Id: ${contact.id}</p>
-          <p>User Name: ${contact.userName}</p>
-          <p>Completed: ${contact.completed ? "Yes" : "No"}</p>
+          <p>User Name: ${contact.name}</p>
+          <p>Completed: ${contact.completed ? 'Yes' : 'No'}</p>
         </div>
       `
     )
-    .join("");
+    .join('')
 
-  const buttonHtml = `<button onclick="location.href='/form'">Add new contact</button>`;
+  const buttonHtml = `<button onclick="location.href='/form'">Add new contact</button>`
 
-  return createHtmlTemplate(`${headerHtml}${contactsHtml}${buttonHtml}`);
-};
+  return createHtmlTemplate(`${headerHtml}${contactsHtml}${buttonHtml}`)
+}
 
-let formTemplate;
+let formTemplate
 
 const loadFormTemplate = async () => {
   try {
-    formTemplate = await fs.readFile("./templates/form.html");
+    formTemplate = await fs.readFile('./templates/form.html')
   } catch (error) {
-    console.error("Error reading form.html file:", error);
+    console.error('Error reading form.html file:', error)
   }
-};
+}
 
-loadFormTemplate();
+loadFormTemplate()
 
 const contacts = [
   {
-    userName: "Alice",
     id: 1,
+    name: 'Alice',
     completed: false
   },
   {
-    userName: "Bob",
     id: 2,
+    name: 'Bob',
     completed: false
   },
   {
-    userName: "Carol",
     id: 3,
+    name: 'Carol',
     completed: false
   },
   {
-    userName: "David",
     id: 4,
+    name: 'David',
     completed: true
   },
   {
-    userName: "Eve",
     id: 5,
+    name: 'Eve',
     completed: false
   }
-];
+]
 
-export {
-  rootHtmlTemplate,
-  notFoundHtmlTemplate,
-  contacts,
-  formTemplate,
-  generateContactsTemplate,
-  createHtmlTemplate
-};
+export { rootHtmlTemplate, notFoundHtmlTemplate, contacts, formTemplate, generateContactsTemplate, createHtmlTemplate }
